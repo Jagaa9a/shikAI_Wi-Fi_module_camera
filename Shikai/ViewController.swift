a0372e977a7663bfca6f0c8a2ef72d86791d7766
Amgalan Jargalsaikhan <amgalanjargalsaikhan@Amgalans-MacBook-Air.local>
2017-02-24 17:26:05 +0900

    First commit

--- Shikai/ViewController.swift
+++ Shikai/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let CameraURL = URL(string: "http://10.10.1.1:8196")
+        let CameraURLRequest = URLRequest(url: CameraURL!)
+        webView.loadRequest(CameraURLRequest)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    @IBAction func Take(_ sender: UIButton) {
+        let layer = UIApplication.shared.keyWindow!.layer
+        let scale = UIScreen.main.scale
+        UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
+        
+        layer.render(in: UIGraphicsGetCurrentContext()!)
+        let screenshot = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        UIImageWriteToSavedPhotosAlbum(screenshot!, nil, nil, nil)
+        webView.reload()
+    }
 
 }
 
